889cba8ad63bc14620c29eee66bd61f1eb7c3ce9
ferrinweb <ferrinweb@gmail.com>
2018-11-29 14:30:25 +0800

    remove unused settings

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,6 @@ const path = require('path')
 module.exports = {
   lintOnSave: true,
   baseUrl: './',
-  // pluginOptions: {
-  //   'style-resources-loader': {
-  //     preProcessor: 'scss',
-  //     patterns: [path.resolve(__dirname, 'src/assets/scss/variables.scss')]
-  //   }
-  // },
   devServer: {
     proxy: {
       '/asr': {
@@ -16,14 +10,6 @@ module.exports = {
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         pathRewrite: {
           '^/asr': ''
-        }
-      },
-      '/api': {
-        target: 'http://192.168.9.129:8088', // 接口的域名
-        // secure: false,  // 如果是https接口，需要配置这个参数
-        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
-        pathRewrite: {
-          '^/api': ''
         }
       }
     }
