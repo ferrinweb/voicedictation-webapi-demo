eb56a9e2eac6162d38fd68ce7d70af616a96edd3
ferrinweb <ferrinweb@gmail.com>
2018-11-29 14:15:42 +0800

    add files

---
+++ src/api/asr-config.js
@@ -0,0 +1,11 @@
+const config = {
+  engineType: 'sms16k', // 引擎类型, 默认即可
+  aue: 'raw', // 音频编码，本实例取'raw'
+  appId: '', // 应用ID
+  APIKey: '', // 服务密钥
+  sampleRate: 16000, // 采样率(48000)，注意：设定的值必须为 48000 的约数
+  sampleBits: 16, // 采样比特率，8 或 16
+  twoChannel: false // 双声道
+}
+
+export default config
